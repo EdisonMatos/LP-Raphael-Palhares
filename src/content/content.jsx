fc681d7b27ca2c6387cf8c4bb1034b1038e75e70
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-06-20 15:10:41 -0300

    fix: Webp imagens

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -1,7 +1,7 @@
 import logo from "../assets/imgs/logo/logo.webp";
 import heroImgPhone from "../assets/imgs/hero/personImg.png";
 import heroImgDesktop from "../assets/imgs/hero/bgHeroInfluencer.jpg";
-import bgHeroImg from "../assets/imgs/hero/bgHeroInfluencer.jpg";
+import bgHeroImg from "../assets/imgs/hero/bgHeroInfluencer.webp";
 import heroDefaultImg from "../assets/imgs/hero/lawHero.webp";
 import backgroundPersonMobile from "../assets/imgs/hero/bgHeroMobile.jpg";
 
