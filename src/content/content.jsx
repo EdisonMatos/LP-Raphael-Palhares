5d240875efec2a0a52eb9395d516b1a8c7aaa2d2
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-07-02 16:15:33 -0300

    fix: Label Botão Hero

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -138,7 +138,7 @@ const content = {
       heroImg: bgHeroImg, //fundo da Hero
       heroDefaultImage: heroDefaultImg, //imagem da heroDefault Mobile
       backgroundPerson: backgroundPersonMobile,
-      ctaButtonText: "Fale com um especialista",
+      ctaButtonText: "Entrar em contato",
       obsHero: {
         icon: (
           <svg
